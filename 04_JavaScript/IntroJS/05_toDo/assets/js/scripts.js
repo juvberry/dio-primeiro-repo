7145dceed149e0b80a5a42d3283653c9e7a5ab21
juvberry <julia.dvb@gmail.com>
2022-07-06 12:02:52 -0300

    desafio contador, falta preencher a lista com as tarefas digitadas no input

---
+++ 04_JavaScript/IntroJS/05_toDo/assets/js/scripts.js
@@ -0,0 +1,32 @@
+const Form = document.getElementById('add-tasks')
+let newTask = document.getElementById('task')
+let btnAdd = document.getElementById('btnAdd')
+let list = document.getElementById('list-id')
+let listArr = list.getElementsByClassName('item-list')
+let arr = JSON.parse(localStorage.getItem("tasks"));
+
+// Form.onsubmit = r => {
+//     addTask()
+// }
+
+btnAdd.addEventListener("click", addTask)
+
+function addTask(){
+    let newTaskValue = newTask.value
+    let a = getTasks(newTaskValue)
+    if(a.length === 0){
+        arr.push(newTaskValue)
+    }
+    
+    localStorage.setItem('tasks', JSON.stringify(arr))
+}
+
+function getTasks(newTaskValue){
+    let newTaskArr = [];
+    if(arr && arr.length > 0){
+        newTaskArr = arr.filter(a => {
+            return a.toLowerCase() == newTaskValue.toLowerCase()
+        })
+    }
+    return newTaskArr
+}
